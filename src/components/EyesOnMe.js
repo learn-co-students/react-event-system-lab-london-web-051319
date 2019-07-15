4d726a9b83b441ad83a9129e836ddd6cd88d4a28
James <hayward.jp@gmail.com>
2019-07-15 17:33:13 +0100

    added comments

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react';
 export default class EyesOnMe extends Component {
   render() {
     return(
-      <button onFocus={() => console.log('Good!')} onBlur={() => console.log("Hey! Eyes on me!")} ></button>
+      <button onFocus={() => console.log('Good!')} onBlur={() => console.log("Hey! Eyes on me!")} ></button> // you must add in the function in these cases because you don't want the events to automatically fire, they must perceieve an event. 
     )
   }
 }
